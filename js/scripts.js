6068cc93db3022c5d038b379e08651e4f9c30331
Lourine <lourine.millicent@gmail.com>
2020-08-03 17:57:14 +0300

    Add User Interface Logic to scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,44 @@ function Address (address) {
   this.address =address;
   this.deliveryAddress = address;
 }
+//User Interface Logic
+$(document).ready(function(event) {
+  /////Landing Page Btns
+    $("#pickup-btn").click(function() {
+      $("#order-content").show();
+      $("#landing-content").hide();
+      $("#delivery-option").text("PICKUP BY CUSTOMER");
+    });
+    $("#delivery-btn").click(function() {
+      $("#address").show();
+      $("#pickup-btn,#delivery-btn,#landing-tagline").hide();
+    });
+    $("form#address-form").submit(function(event) {
+      event.preventDefault();
+      var address = $("input#add").val();
+      var newAddress = new Address(address)
+      $("#order-content").show();
+      $("#landing-content").hide();
+      $("#delivery-option").text("Your Pizza will be delivered to: " + newAddress.deliveryAddress);
+    });
+    $("form#custom-pizza").submit(function(event) {
+      event.preventDefault();
+      var flavor= $("select#flavor").val();
+      var customSize = $("select#size").val();
+      var crust = $("select#crust").val();
+      var veggie1 = $("select#veggie1").val();
+      var veggie2 = $("select#veggie2").val();
+      var meat = $("select#meat").val();
+      var pizzaDetails = (flavor + ", "+ customSize +  ", " + crust + ", " + veggie1 + ", " + veggie2 + ", " + meat);
+      var newPizzaOrder = new Order(flavor, customSize, crust);
+      newPizzaOrder.pizzaCost();
+      totalPriceArray.push(newPizzaOrder.pizzaPrice);
+      $("#pizza-details-dropdown").show();
+      $("#final-cost").text(newPizzaOrder.finalCost());
+      $("#pizza-details").append("<ul><li>" + pizzaDetails + "</li></ul>");
+      $("# flavor #size, #crust, #veggie1, #veggie2, #meat").val("");
+    });
+    $("#pizza-details-dropdown").click(function() {
+      $("#pizza-details").toggle();
+    });
+  
